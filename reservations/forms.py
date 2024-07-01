d202e194672a4ef97238d7f500565807510e595b
Pramila Shanmugam <pramila.sl@gmail.com>
2024-07-01 16:53:27 +0000

    modified forms and models, included validation for date and time

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,7 +1,27 @@
 from django import forms
-from .models import Reservation
+from django.contrib import admin
+from .models import Reservation, Table
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
-        fields = ['table', 'date', 'time', 'guests', 'phone', 'notes']
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['time'].widget = forms.Select(choices=[
+            ('12:00', '12:00 PM'), ('12:15', '12:15 PM'), ('12:30', '12:30 PM'), ('12:45', '12:45 PM'),
+            ('13:00', '1:00 PM'), ('13:15', '1:15 PM'), ('13:30', '1:30 PM'), ('13:45', '1:45 PM'),
+            ('14:00', '2:00 PM'), ('14:15', '2:15 PM'), ('14:30', '2:30 PM'),
+            ('17:30', '5:30 PM'), ('17:45', '5:45 PM'), ('18:00', '6:00 PM'), ('18:15', '6:15 PM'),
+            ('18:30', '6:30 PM'), ('18:45', '6:45 PM'), ('19:00', '7:00 PM'), ('19:15', '7:15 PM'),
+            ('19:30', '7:30 PM'), ('19:45', '7:45 PM'), ('20:00', '8:00 PM'), ('20:15', '8:15 PM'),
+            ('20:30', '8:30 PM'), ('20:45', '8:45 PM'), ('21:00', '9:00 PM'), ('21:15', '9:15 PM'),
+            ('21:30', '9:30 PM'), ('21:45', '9:45 PM'), ('22:00', '10:00 PM')
+        ])
+
+class ReservationAdmin(admin.ModelAdmin):
+    form = ReservationForm
+
+admin.site.register(Reservation, ReservationAdmin)
+admin.site.register(Table)
--- reservations/models.py
+++ reservations/models.py
@@ -1,7 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import date
+from django.utils import timezone
+import datetime
 
 class Table(models.Model):
     number = models.IntegerField(unique=True)
@@ -10,15 +11,32 @@ class Table(models.Model):
     def __str__(self):
         return f'Table {self.number} (Capacity: {self.capacity})'
 
-# validating the booking time to match the restaurant open time
 def validate_reservation_time(value):
-    if not ((datetime.time(12, 0) <= value <= datetime.time(14, 30)) or (datetime.time(17, 30) <= value <= datetime.time(22, 0))):
-        raise ValidationError("Reservation time must be between 12:00 to 14:30 or 17:30 to 22:00.")
+    allowed_times = [
+        datetime.time(hour=12, minute=0), datetime.time(hour=12, minute=15), datetime.time(hour=12, minute=30),
+        datetime.time(hour=12, minute=45), datetime.time(hour=13, minute=0), datetime.time(hour=13, minute=15),
+        datetime.time(hour=13, minute=30), datetime.time(hour=13, minute=45), datetime.time(hour=14, minute=0),
+        datetime.time(hour=14, minute=15), datetime.time(hour=14, minute=30), datetime.time(hour=17, minute=30),
+        datetime.time(hour=17, minute=45), datetime.time(hour=18, minute=0), datetime.time(hour=18, minute=15),
+        datetime.time(hour=18, minute=30), datetime.time(hour=18, minute=45), datetime.time(hour=19, minute=0),
+        datetime.time(hour=19, minute=15), datetime.time(hour=19, minute=30), datetime.time(hour=19, minute=45),
+        datetime.time(hour=20, minute=0), datetime.time(hour=20, minute=15), datetime.time(hour=20, minute=30),
+        datetime.time(hour=20, minute=45), datetime.time(hour=21, minute=0), datetime.time(hour=21, minute=15),
+        datetime.time(hour=21, minute=30), datetime.time(hour=21, minute=45), datetime.time(hour=22, minute=0)
+    ]
+    if value not in allowed_times:
+        raise ValidationError("Reservation time must be one of the allowed slots.")
+
+def validate_reservation_date(value):
+    today = timezone.now().date()
+    six_months_from_today = today + datetime.timedelta(days=6*30)  # Approximation for 6 months
+    if not (today <= value <= six_months_from_today):
+        raise ValidationError("Reservation date must be between today and the next 6 months.")
 
 class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
-    date = models.DateField(default=date.today)  # This will set the date to the current date by default
+    date = models.DateField(default=timezone.now, validators=[validate_reservation_date])
     time = models.TimeField(validators=[validate_reservation_time])
     guests = models.IntegerField(default=0)
     phone = models.IntegerField()
