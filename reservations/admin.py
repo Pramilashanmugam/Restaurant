01b2e7bf5d776afa618f6539113acddb7f18b3d3
Pramila Shanmugam <pramila.sl@gmail.com>
2024-07-11 21:44:56 +0000

    add js directory, css customisation for menu, included user &  table in admin filter, updated li to p i reservation_list

--- reservations/admin.py
+++ reservations/admin.py
@@ -5,7 +5,7 @@ from .models import Reservation, Table
 # This class is to customize how the Reservation model to be displayed in the Django admin
 class ReservationAdmin(admin.ModelAdmin):
     list_display = ('user', 'table', 'date', 'time', 'guests', 'phone')
-    list_filter = ('date', 'time')
+    list_filter = ('date', 'time', 'user', 'table')
     search_fields = ('user__username', 'table__number', 'phone')
 
 # Register your models here.
